ee4a502b658b3f1900a357546638d8d0f567fa65
Khamal Ismadie <37945835+khamalismadie@users.noreply.github.com>
2022-06-12 00:54:11 +0700

    Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,35 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let marah = 0;
+  let tidakMarah = 0;
+
+  let dataIXX = await promiseTheaterIXX();
+  dataIXX.forEach((object, index) => {
+    if (object.hasil === "marah") {
+      marah += 1;
+    }
+    if (object.hasil === "tidak marah") {
+      tidakMarah += 1;
+    }
+  });
+
+  let dataVGC = await promiseTheaterVGC();
+  dataVGC.forEach((object, index) => {
+    if (object.hasil === "marah") {
+      marah += 1;
+    }
+    if (object.hasil === "tidak marah") {
+      tidakMarah += 1;
+    }
+  });
+  if (emosi === "marah") {
+    return marah;
+  } else if (emosi === "tidak marah") {
+    return tidakMarah;
+  }
+};
 
 module.exports = {
   promiseOutput,
